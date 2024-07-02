6f4b9e7474f1a9e5609de3bc4f16701da6a2aa91
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-02 13:30:43 +0200

    - DEBUG settings.py set up with conditional True for dev and False for prod

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application # This is the WSGI_APPLICATION from settings.py. Here its a : IMPORTANT
