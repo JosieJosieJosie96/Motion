907e9677343538ab08f528ddcb9c60338bf24390
Tim Groshaupt <timgroshaupt@googlemail.com>
2024-07-02 10:29:23 +0200

    - gunicorn installed and implemented in the prod.sh

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 python manage.py makemigrations
-python manage.py runserver 0:8000
+gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application # This is the WSGI_APPLICATION from settings.py. Here its a : IMPORTANT
