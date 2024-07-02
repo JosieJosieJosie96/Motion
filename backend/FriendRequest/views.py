435fd475766fca18507443b19fdb921864958f06
Sabin Tamang <tamang.sabin.np@gmail.com>
2024-07-02 11:30:33 +0200

    filter added in req.file

--- backend/FriendRequest/views.py
+++ backend/FriendRequest/views.py
@@ -1,8 +1,16 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
 from .models import FriendRequest
 from .serializers import FriendRequestSerializer
 
 
-class FriendRequestListView(ListCreateAPIView):
+class FriendRequestListView(GenericAPIView):
     queryset = FriendRequest.objects.all()
     serializer_class = FriendRequestSerializer
+
+    def get(self, request, *args, **kwargs):
+        requests = FriendRequest.objects.filter(user_friends=request.user)
+        serializer = self.get_serializer(requests, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
