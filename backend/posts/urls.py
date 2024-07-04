7379ae3cfa3c8a07b1902f9d90e596972a2f5b19
Josie Fessey <07jreckn@gmail.com>
2024-07-04 18:26:46 +0300

    finished model and views and serializers

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from posts.views import ListSearchPosts, RetrieveUpdateDestroyPost, ListPostsUser, ListLikes, ListPostsFollowees, \
-    CreateLike, ListCreatePosts
+    CreateLike, ListCreatePosts, ListFriendsPostsView
 
-from posts.views import ListCreatePosts
 
 urlpatterns = [
     path('', ListCreatePosts.as_view(), name='post-list'),
@@ -10,7 +9,7 @@ urlpatterns = [
     path('<int:post_id>/', RetrieveUpdateDestroyPost.as_view(), name='retrieve-update-destroy-post'),
     path('<int:user_id>/', ListPostsUser.as_view(), name='list-posts-user'),
     path("following/", ListPostsFollowees.as_view(), name="list-posts-followees"),
-    # path("friends/", .as_view(), name="friends"),
+    path("friends/", ListFriendsPostsView.as_view(), name="friends-posts"),
     path("likes/", ListLikes.as_view(), name="list-liked-posts"),
     path("toggle-like/<int:post_id>/", CreateLike.as_view(), name="toggle-like")
 
